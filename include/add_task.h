677455be498795869e01aace8a45d7cf5a4a1e44
𝙙𝙞𝙭𝙚 <135438002+dixe1@users.noreply.github.com>
2025-10-12 14:21:59 +0200

    added description for tasks

--- include/add_task.h
+++ include/add_task.h
@@ -1,6 +1,6 @@
 #pragma once
 
 void addTask();
-void getUserData(std::string &taskName, std::string taskEndDate[]);
+void getUserData(std::string &taskName, std::string &taskDescription, std::string taskEndDate[]);
 std::string formatTime(time_t t);
 extern std::string alertCode;
